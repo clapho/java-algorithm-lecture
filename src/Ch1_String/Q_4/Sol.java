35bb57deb35386f8e8e84a0fde5c4a0bf96127d6
SoohoPark <clapp301@gmail.com>
2023-09-13 16:24:44 +0900

    [Ch1] 단어 뒤집기

---
+++ src/Ch1_String/Q_4/Sol.java
@@ -0,0 +1,45 @@
+package Ch1_String.Q_4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// StringBuilder
+public class Sol {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+//        for (String s : str) {
+//            String tmp = new StringBuilder(s).reverse().toString();
+//            answer.add(tmp);
+//        }
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Sol T = new Sol();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+
+}
