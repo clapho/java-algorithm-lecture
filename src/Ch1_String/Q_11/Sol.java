0a2e8211429c6f8a24d940724c4103783af8f73a
SoohoPark <clapp301@gmail.com>
2023-09-20 15:00:06 +0900

    [Ch1] 문자열 압축

---
+++ src/Ch1_String/Q_11/Sol.java
@@ -0,0 +1,29 @@
+package Ch1_String.Q_11;
+
+import java.util.Scanner;
+
+public class Sol {
+
+    public String solution(String s) {
+        String answer = "";
+        s = s + " ";
+        int cnt = 1;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) == s.charAt(i+1)) cnt++;
+            else {
+                answer += s.charAt(i);
+                if(cnt > 1) answer += String.valueOf(cnt);
+                cnt = 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sol T = new Sol();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
