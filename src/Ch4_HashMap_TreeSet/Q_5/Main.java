1e1d2e1b097d13184ade92e801ab1cb7477bdcb7
SoohoPark <clapp301@gmail.com>
2023-10-16 17:27:41 +0900

    [Ch4] K번째 큰 수

---
+++ src/Ch4_HashMap_TreeSet/Q_5/Main.java
@@ -0,0 +1,46 @@
+package Ch4_HashMap_TreeSet.Q_5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public int solution(int n, int k, int[] arr) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int l = j+1; l < n; l++) {
+                    set.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list, Collections.reverseOrder());
+
+        if (list.size() < k) {
+            return -1;
+        }
+
+        return list.get(k-1);
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.println(T.solution(n, k, arr));
+    }
+}
