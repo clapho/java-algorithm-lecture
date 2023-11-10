1908d9178c4f492b6527977268f124d04ba4f8a3
SoohoPark <clapp301@gmail.com>
2023-11-10 15:41:27 +0900

    [Ch6] 삽입정렬

---
+++ src/Ch6_Sorting_Searching/Q_3/Sol.java
@@ -0,0 +1,29 @@
+package Ch6_Sorting_Searching.Q_3;
+
+import java.util.Scanner;
+
+public class Sol {
+
+    public int[] solution(int n, int[] arr) {
+
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i], j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > tmp) arr[j+1] = arr[j];
+                else break;
+            }
+            arr[j+1] = tmp;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Sol T = new Sol();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+}
