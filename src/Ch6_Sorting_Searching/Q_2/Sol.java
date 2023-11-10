80f49417d07d5d6c114924e05df92ace575c16dd
SoohoPark <clapp301@gmail.com>
2023-11-10 15:12:29 +0900

    [Ch6] 버블정렬

---
+++ src/Ch6_Sorting_Searching/Q_2/Sol.java
@@ -0,0 +1,29 @@
+package Ch6_Sorting_Searching.Q_2;
+
+import java.util.Scanner;
+
+public class Sol {
+
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Sol T = new Sol();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+}
